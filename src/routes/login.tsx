c9336cde2f57f9271f602e1189007df225aabda1
ranaqaboos <106505615+ranaqaboos@users.noreply.github.com>
2022-07-28 17:41:58 +0300

    done

---
+++ src/routes/login.tsx
@@ -0,0 +1,10 @@
+import{Login} from '../components/login'
+import { Layout } from '../components/Layout';
+
+export function Loginpage() {
+	return(
+        <Layout>
+        <Login/>
+        </Layout>
+        ) 
+    }
